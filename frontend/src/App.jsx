74518377888d642b076a4c2d8302f22295ea432b
Vimal <vm.21cs48@thdcihet.ac.in>
2024-08-25 08:40:01 +0530

    basic setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import Login from './pages/login/Login'
 import './App.css'
 import Signup from './pages/signup/Signup.jsx'
+import Home from './pages/home/Home.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-    <Signup/>
+    <Home/>
     </div>
   )
 }
